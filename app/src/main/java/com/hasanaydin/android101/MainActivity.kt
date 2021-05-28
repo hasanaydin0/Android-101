fbd3286cd4990831d8648796a78b1d1a3a9dcdf4
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-05-29 00:50:19 +0300

    sum-function-UI

--- app/src/main/java/com/hasanaydin/android101/MainActivity.kt
+++ app/src/main/java/com/hasanaydin/android101/MainActivity.kt
@@ -2,13 +2,24 @@ package com.hasanaydin.android101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        button.setOnClickListener {
+            val result =  sum(number1.text.toString().toInt(),number2.text.toString().toInt())
+            sonuc.text = "Sonuc = $result"
+        }
 
     }
+
+    fun sum (x:Int,y:Int):Int{
+        val z = x+y
+        println(z)
+        return z
+    }
+
 }
